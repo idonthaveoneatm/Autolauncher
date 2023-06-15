a97121b32cb8b05c1f6b65cf7c77c53679af6108
idonthaveoneatm <111616655+idonthaveoneatm@users.noreply.github.com>
2023-06-14 23:16:18 -0400

    Add files via upload

---
+++ checker.py
@@ -0,0 +1,16 @@
+import psutil
+import subprocess as sp
+import keyboard
+import time as t
+print("griffindoescooking: \nHello and thank you for using my relauncher if the button is clicking in the right spot edit the main.bat file")
+print("\nNow we wait for it to crash...")
+def checkblox():
+	for proc in psutil.process_iter(['pid','name']):
+		if proc.info['name'] == 'Windows10Universal.exe':
+			return False
+	print("Roblox Crashed!")
+	return True
+while keyboard.is_pressed('q') == False:
+	if checkblox():
+		t.sleep(5)
+		sp.call(['main.bat'])
---
+++ main.bat
@@ -0,0 +1,14 @@
+:if you need to get your mouse position run mouse.bat position in terminal
+
+call mouse.bat moveTo 615x790
+:This launches roblox
+call mouse.bat click
+timeout /t 15 /nobreak
+call mouse.bat moveTo 397x605
+:This clicks PSX
+call mouse.bat click
+timeout /t 3 /nobreak
+call mouse.bat moveTo 628x464
+:This clicks play
+call mouse.bat click
+echo relaunched
