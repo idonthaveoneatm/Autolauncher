cfc94188f86945b8bbb60476320ae184f912d62f
idonthaveoneatm <111616655+idonthaveoneatm@users.noreply.github.com>
2023-06-15 01:27:23 -0400

    Update checker.py

--- checker.py
+++ checker.py
@@ -2,7 +2,7 @@ import psutil
 import subprocess as sp
 import keyboard
 import time as t
-print("griffindoescooking: \nHello and thank you for using my relauncher if the button is clicking in the right spot edit the main.bat file")
+print("griffindoescooking: \nHello and thank you for using my relauncher if the button isnt clicking in the right spot edit the main.bat file")
 print("\nNow we wait for it to crash...")
 def checkblox():
 	for proc in psutil.process_iter(['pid','name']):
@@ -12,5 +12,4 @@ def checkblox():
 	return True
 while keyboard.is_pressed('q') == False:
 	if checkblox():
-		t.sleep(5)
 		sp.call(['main.bat'])
