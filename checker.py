10ddf402c31406a7b3d4ab3ca8b3adfefcb4623f
idonthaveoneatm <111616655+idonthaveoneatm@users.noreply.github.com>
2023-06-16 15:48:43 -0400

    Update checker.py

--- checker.py
+++ checker.py
@@ -1,15 +1,33 @@
 import psutil
 import subprocess as sp
 import keyboard
-import time as t
+
+WEBHOOK = False #Toggle True or False
+
+if WEBHOOK:
+	from discord_webhook import DiscordWebhook, DiscordEmbed
+	#Discord webhook information
+	discordid = "" #If you dont know how to get it google it
+	webhookurl = "" #Your webhook
+	
+	content = f"<@!{discordid}>"
+	allowed_mentions = {"users": [f"{discordid}"]}
+	webhook = DiscordWebhook(url=webhookurl,content=content,allowed_mentions=allowed_mentions)
+	webhook.add_embed(DiscordEmbed(title='Game Restarted',description='The game shouldve restarted for you',color='a49ae6'))
+
+
 print("griffindoescooking: \nHello and thank you for using my relauncher if the button isnt clicking in the right spot edit the main.bat file")
 print("\nNow we wait for it to crash...")
+
 def checkblox():
 	for proc in psutil.process_iter(['pid','name']):
 		if proc.info['name'] == 'Windows10Universal.exe':
 			return False
 	print("Roblox Crashed!")
 	return True
+	
 while keyboard.is_pressed('q') == False:
 	if checkblox():
 		sp.call(['main.bat'])
+		if WEBHOOK: 
+			response = webhook.execute()
